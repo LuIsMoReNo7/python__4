079ef7c0082ab3eb1eea0c58311234d0b99dbb3b
LuIsMoReNo7 <1483853@senati.pe>
2023-03-03 09:55:15 -0500

    Hallamos el area del circulo dandole valor al Radio e importado el valor de pi mediante la funcion import

---
+++ Area del circulo.py
@@ -0,0 +1,13 @@
+#importamos el valor de pi 
+import math
+
+#le damos un valor al radio
+R = int(input("asignamos un valor al R :"))
+
+#Hallamos el valor del area
+area = math.pi * (R * R)
+
+print("el area del circulo con radio ",R," es: ",area)
+
+
+
