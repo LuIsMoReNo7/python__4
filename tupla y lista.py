7b801b255531995304d589c9a572c90d7a191bfc
LuIsMoReNo7 <1483853@senati.pe>
2023-03-03 11:47:48 -0500

    usamos el map y str para transformar y manipular

---
+++ tupla y lista.py
@@ -0,0 +1,10 @@
+#algoitmo que permita ingresar datos
+Numeros = 10, 15, 27, 100, 200
+#usamos el map para  transformar un iterable completo usando otra función
+#usamos el string (str) para representar y manipular una secuencia de caracteres
+lista = list(map(str, Numeros))
+tupla = tuple(map(str, Numeros))
+#imprimimos los valores
+print(lista)
+print(tupla)
+
