577b86f04024be75f2e6a4f6f491707942c55d1d
LuIsMoReNo7 <1483853@senati.pe>
2023-03-03 11:02:47 -0500

    le agregre el round para redondear el valor

--- Area del circulo.py
+++ Area del circulo.py
@@ -7,7 +7,7 @@ R = int(input("asignamos un valor al R :"))
 #Hallamos el valor del area
 area = math.pi * (R * R)
 
-print("el area del circulo con radio ",R," es: ",area)
+print("el area del circulo con radio ",R," es: ",round(area, 2))
 
 
 
