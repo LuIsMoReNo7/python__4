fdf8628733143cb9a2fc915ac348d7188b47d7e6
LuIsMoReNo7 <1483853@senati.pe>
2023-03-04 09:49:42 -0500

    un cambio

--- lista letras.py
+++ lista letras.py
@@ -1,4 +1,4 @@
 Lista = ["red", "blue", "yellow", "white"]
-#usamos los numeros negaivos para seleccionar los elementos de la lista
-print('el primer elemento de la lista:', Lista[-4])
+#usamos los numeros negativos para seleccionar los elementos de la lista
+print('el primer elemento de la lista:', Lista[0])
 print('el ultimo elemento de la lista:', Lista[-1])
