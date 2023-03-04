8fc38bb79ea9002cc5d45c1f0ac6d967d30a6b2b
LuIsMoReNo7 <1483853@senati.pe>
2023-03-04 09:30:04 -0500

    lista de elementos

---
+++ lista letras.py
@@ -0,0 +1,4 @@
+Lista = ["red", "blue", "yellow", "white"]
+#usamos los numeros negaivos para seleccionar los elementos de la lista
+print('el primer elemento de la lista:', Lista[-4])
+print('el ultimo elemento de la lista:', Lista[-1])
